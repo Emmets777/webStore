819836973193a0d7d57a31e876e54d92ef8306ea
Emmet <emmetdev2209@gmail.com>
2026-05-12 21:34:50 +0000

    style: styling the header component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,11 @@
 import "./css/Footer.css";
 
 function Footer() {
-    <>
+    return (
         <footer>
             <h4>be back!</h4>
         </footer>
-    </>
+    )
 }
 
 export default Footer;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,23 +1,43 @@
 import "./css/Header.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { FaCode, FaUserCircle, FaCog } from "react-icons/fa";
 
 function Header() {
-    <>
-        <header>
-            <nav>
-                <FontAwesomeIcon icon="fa-solid fa-code" />
-                <ul>
-                    <li><a href="/">Home</a></li>
-                    <li><a href="/about">About</a></li>
-                    <li><a href="/contact">Contact</a></li>
-                </ul>
-                <ul>
-                    <li><a href="/login">Login</a></li>
-                    <li><a href="/settings">Settings</a></li>
-                </ul>
-            </nav>
-        </header>
-    </>
+  return (
+    <header>
+      <nav>
+        <FaCode className="logo" />
+        <ul className="nav-links">
+          <li>
+            <a href="#">
+              Home
+            </a>
+          </li>
+          <li>
+            <a href="/about">
+              About
+            </a>
+          </li>
+          <li>
+            <a href="/contact">
+              Contact
+            </a>
+          </li>
+        </ul>
+        <ul className="utils">
+          <li>
+            <a href="/login">
+              <FaUserCircle className="utils-icon" />
+            </a>
+          </li>
+          <li>
+            <a href="/settings">
+              <FaCog className="utils-icon" />
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
 
 export default Header;
