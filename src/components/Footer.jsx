b9abad35d5fd095b9788c216838e01c796e85374
Emmet <emmetdev2209@gmail.com>
2026-05-13 19:25:23 +0000

    feat: creating the footer section and style

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,46 @@
 import "./css/Footer.css";
+import { MdEmail } from "react-icons/md";
+import { FaGithub, FaHome } from "react-icons/fa";
 
 function Footer() {
-    return (
-        <footer>
-            <h4>be back!</h4>
-        </footer>
-    )
+  return (
+    <footer>
+      <div className="wrapper">
+        <div className="tks">
+          <p>
+            i would thank you for your prefference in our store, we hope you
+            found what you are searching for, if you had any suggestion for our
+            team to improve your experience or suggestion for new products, fell
+            welcome to send a email to our team! come back anytime!
+          </p>
+        </div>
+        <section id="links">
+          <p>
+            <a href="#">
+              <MdEmail />
+              Contact us
+            </a>
+          </p>
+          <p>
+            <a href="https://github.com/Emmets777">
+              <FaGithub />
+              Created by
+            </a>
+          </p>
+          <p>
+            <a href="./home">
+              <FaHome />
+              Home
+            </a>
+          </p>
+        </section>
+        <p id="rights">
+          All Rights Reserved © 2024 - 2026
+          <a href="www.linkedin.com/in/fabio-luiz-269501225/">Fabio Luiz</a>.
+        </p>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
